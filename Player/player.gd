ec1c113d09e5093cd01dd3ae28d1d82bd41000d1
Henry Pham <henrypham6719@gmail.com>
2024-10-18 19:22:21 -0500

    Add ShadowSpear

--- Player/player.gd
+++ Player/player.gd
@@ -2,8 +2,28 @@ extends CharacterBody2D
 
 var movement_speed = 40.0
 var hp = 80
+
+#attack
+var shadowSpear = preload("res://Player/Attack/shadow_spear.tscn")
+
+#attackNodes
+@onready var shadowSpearTimer = get_node("%ShadowSpearTimer")
+@onready var shadowSpearAttackTimer = get_node("%ShadowSpearAttackTimer")
+
+#shadowSpear
+var shadowspear_ammo = 0
+var shadowspear_baseammo = 1
+var shadowspear_attackspeed = 1.5
+var shadowspear_level = 1
+
+#enemy related
+var enemy_close = []
+
 @onready var sprite = $Sprite2D
 @onready var walkTimer = get_node("%walkTimer")
+
+func _ready():
+	attack()
 
 func _physics_process(delta):
 	movement()
@@ -30,8 +50,49 @@ func movement():
 	velocity = mov.normalized()*movement_speed
 	move_and_slide()
 	
-
+func attack():
+	if shadowspear_level > 0:
+		shadowSpearTimer.wait_time = shadowspear_attackspeed
+		if shadowSpearTimer.is_stopped():
+			shadowSpearTimer.start()
 
 func _on_hurt_box_hurt(damage):
 	hp -= damage
 	print(hp)
+
+
+func _on_shadow_spear_timer_timeout() -> void:
+	shadowspear_ammo += shadowspear_baseammo
+	shadowSpearAttackTimer.start()
+
+
+func _on_shadow_spear_attack_timer_timeout() -> void:
+	if shadowspear_ammo > 0:
+		var shadowspear_attack = shadowSpear.instantiate()
+		shadowspear_attack.position = position
+		shadowspear_attack.target = get_random_target()
+		shadowspear_attack.level = shadowspear_level
+		add_child(shadowspear_attack)
+		shadowspear_ammo -= 1
+		if shadowspear_ammo > 0:
+			shadowSpearAttackTimer.start()
+		else: 
+			shadowSpearAttackTimer.stop()
+	
+		
+func get_random_target():
+	if enemy_close.size() > 0:
+		return enemy_close.pick_random().global_position
+	else:
+		return Vector2.UP
+		
+
+
+func _on_enemy_detection_area_body_entered(body: Node2D) -> void:
+	if not enemy_close.has(body):
+		enemy_close.append(body)
+
+
+func _on_enemy_detection_area_body_exited(body: Node2D) -> void:
+	if enemy_close.has(body):
+		enemy_close.erase(body)
